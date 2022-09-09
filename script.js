8a7fa01c2051d4ee2c885a17401ff6af560fbe6d
Abdullokh_dev <101015366+Abdullokh-dev@users.noreply.github.com>
2022-09-09 20:23:57 +0500

    Music Player with JS

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
+const image = document.querySelector('img')
+const title = document.getElementById('title')
+const artist = document.getElementById('artist')
 const music = document.querySelector('audio')
 const prevBtn = document.getElementById('prev')
 const playBtn = document.getElementById('play')
 const nextBtn = document.getElementById('next')
+
+//Music
+const songs = [
+    {
+        name: 'jacinto-1',
+        displayName: 'Voodoo',
+        artist: 'Badshah, J Balvin & Tainy'
+    },
+    {
+        name: 'jacinto-2',
+        displayName: 'VOODOO',
+        artist: 'WILLY WILLIAM'
+    },
+    {
+        name: 'jacinto-3',
+        displayName: 'Завязывай',
+        artist: 'GUNWEST'
+    },
+]
 
 let isPlaying = false
 
@@ -20,3 +42,42 @@ function pauseSong() {
 }
 
 playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()))
+
+// Update DOM
+function loadSong(song) {
+    title.textContent = song.displayName
+    artist.textContent = song.artist
+    music.src = `music/${song.name}.mp3`
+    image.src = `img/${song.name}.jpg`
+}
+
+// Current song
+let songIndex = 0
+
+// Previous Song
+function prevSong() {
+    songIndex--
+    if(songIndex < 0) {
+        songIndex = songs.length -1
+    }
+    loadSong(songs[songIndex])
+    playSong()
+}
+
+// Next Song
+function nextSong() {
+    songIndex++
+    if(songIndex > songs.length -1) {
+        songIndex = 0
+    }
+    loadSong(songs[songIndex])
+    playSong()
+}
+
+// On Load Select first Song
+loadSong(songs[songIndex]);
+
+// Events
+prevBtn.addEventListener('click', prevSong)
+
+nextBtn.addEventListener('click', nextSong)
