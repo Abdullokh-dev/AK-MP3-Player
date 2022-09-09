2ef0daeda0b92da07275ee494c12e497dd7172f4
Abdullokh_dev <101015366+Abdullokh-dev@users.noreply.github.com>
2022-09-09 20:34:15 +0500

    progress Bar

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const image = document.querySelector('img')
 const title = document.getElementById('title')
 const artist = document.getElementById('artist')
 const music = document.querySelector('audio')
+const progressContainer = document.getElementById('progress-container')
+const progress = document.getElementById('progress')
 const prevBtn = document.getElementById('prev')
 const playBtn = document.getElementById('play')
 const nextBtn = document.getElementById('next')
@@ -77,7 +79,18 @@ function nextSong() {
 // On Load Select first Song
 loadSong(songs[songIndex]);
 
+//Update ProgressBar and time
+function updateProgressBar(e) {
+    if(isPlaying) {
+        const { duration, currentTime } = e.srcElement;
+        console.log(duration, currentTime)
+
+        const progressPercent = (currentTime / duration) * 100
+        progress.style.width = `${progressPercent}%`
+    }
+}
+
 // Events
 prevBtn.addEventListener('click', prevSong)
-
 nextBtn.addEventListener('click', nextSong)
+music.addEventListener('timeupdate', updateProgressBar)
